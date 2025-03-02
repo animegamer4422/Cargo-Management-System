07ed5a1f2801eed5956fda1d94192b6e871c7e85
animegamer4422 <animegamer4422@gmail.com>
2025-03-03 04:01:40 +0530

    added table recreation to sql schema

--- database_schema.sql
+++ database_schema.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `admin`
 --
 
+DROP DATABASE IF EXISTS cargo_db;
+CREATE DATABASE cargo_db;
+USE cargo_db;
+
 DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
